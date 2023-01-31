d3ef3d1103fa520117ed2378e0916977949048e5
ghorpade.t <ghorpade.t@northeastern.edu>
2023-01-31 03:51:23 -0500

    pwd encryption added

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,9 +4,12 @@ import com.example.demo.ErrorHandeling.UserStatus;
 import com.example.demo.Repository.UserRepository;
 import com.example.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 import java.util.List;
 
@@ -15,7 +18,11 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User saveUser(User user){
+       user.setPassword(passwordEncoder.encode(user.getPassword()));
        return userRepository.save(user);
     }
 
