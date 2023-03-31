2aeedf23a3cc1ac4388d812502c0d6ca1728d6d8
Trisha Ghorpade <98073016+ghorpadetrisha16@users.noreply.github.com>
2023-03-30 22:48:49 -0400

    assignment 7 (#31)

--- src/main/java/com/example/demo/controller/ImageController.java
+++ src/main/java/com/example/demo/controller/ImageController.java
@@ -9,6 +9,7 @@ import com.example.demo.exception.UserAuthrizationExeception;
 import com.example.demo.model.Products;
 import com.example.demo.service.ImageService;
 import com.example.demo.service.ProductService;
+import com.timgroup.statsd.StatsDClient;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -21,38 +22,48 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class ImageController {
-
+    private final static Logger LOGGER = LoggerFactory.getLogger(ImageController.class);
     @Autowired
     ImageService imageService;
     @Autowired
     ProductService productService;
     @Autowired
     private AmazonS3 s3client;
+    @Autowired
+    private StatsDClient statsDClient;
 
     //get all images under productid
     @RequestMapping(path = "/v1/product/{productId}/image", method = RequestMethod.GET)
     public ResponseEntity<?> getImagesByProductID(@PathVariable int productId, HttpServletRequest request) {
+        statsDClient.incrementCounter("v1.product.productId.image.get");
         try {
             if(productId == 0) {
                 throw new InvalidInputException("Enter Valid Product Id");
             }
             productService.isAuthorisedForPut(productId,request.getHeader("Authorization").split(" ")[1], null);
+            LOGGER.info("Authentication successful in ImageController:getImagesByProductID");
             return new ResponseEntity<>(imageService.fetchAllImageByProductId(productId), HttpStatus.OK);
         }catch (UserAuthrizationExeception e) {
+            LOGGER.warn("UserAuthrizationExeception for productId: "+productId+" in ImageController:getImagesByProductID. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.FORBIDDEN);
         }catch (DataNotFoundExeception e) {
+            LOGGER.warn("No Image/product found with given details and productId: "+productId+" in ImageController:getImagesByProductID. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.NOT_FOUND);
         }catch (InvalidInputException e) {
+            LOGGER.warn("Invalid Input Exception for productid: "+productId+" in ImageController:getImagesByProductID. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.BAD_REQUEST);
         }
         catch(Exception e) {
             if (e instanceof DataIntegrityViolationException) {
-                //handle Internal server error 500
+                LOGGER.warn("Invalid required valid data for productId: "+productId+" in ImageController:getImagesByProductID. Exception: "+e);
                 return new ResponseEntity<String>( "Enter Valid data. Make sure SKU is unique",HttpStatus.BAD_REQUEST);
             }
+            LOGGER.error("Internal server error in ImageController:getImagesByProductID. Exception = {}",e);
             return new ResponseEntity<String>(UserConstants.InternalErr,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -60,24 +71,30 @@ public class ImageController {
     //post image
     @PostMapping("/v1/product/{productId}/image")
     public ResponseEntity<?> addImage(@PathVariable int productId, @RequestParam(value = "file") MultipartFile file, HttpServletRequest request) throws Exception {
+        statsDClient.incrementCounter("v1.product.productId.image.post");
         try {
             if(productId == 0 || file == null || file.equals(null) || file.isEmpty()) {
                 throw new InvalidInputException("Enter Valid required data");
             }
             productService.isAuthorisedForPut(productId,request.getHeader("Authorization").split(" ")[1], null);
+            LOGGER.info("Authentication successful for productId: "+productId+" in ImageController:addImage");
             return new ResponseEntity<>(imageService.addFile(productId, file), HttpStatus.CREATED);
         }catch (UserAuthrizationExeception e) {
+            LOGGER.warn("UserAuthrizationExeception for productId: "+productId+" in ImageController:addImage. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.FORBIDDEN);
         }catch (DataNotFoundExeception e) {
+            LOGGER.warn("No Image/product found with given details for productId: "+productId+" in ImageController:addImage. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.NOT_FOUND);
         }catch (InvalidInputException e) {
+            LOGGER.warn("Invalid Input Exception for productId: "+productId+" in ImageController:addImage. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.BAD_REQUEST);
         }
         catch(Exception e) {
             if (e instanceof DataIntegrityViolationException) {
-                //handle Internal server error 500
+                LOGGER.warn("Enter all required valid data for productId: "+productId+" in ImageController:addImage. Exception: "+e);
                 return new ResponseEntity<String>( "Enter Valid data. Make sure SKU is unique",HttpStatus.BAD_REQUEST);
             }
+            LOGGER.error("Internal server error in ImageController:addImage. Exception = {}",e);
             return new ResponseEntity<String>(UserConstants.InternalErr,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -85,24 +102,30 @@ public class ImageController {
     //get image by product and image id
     @RequestMapping(path = "/v1/product/{productId}/image/{image_id}", method = RequestMethod.GET)
     public ResponseEntity<?> fetchImageByID(@PathVariable int productId, @PathVariable int image_id, HttpServletRequest request) {
+        statsDClient.incrementCounter("v1.product.productId.image.image_id.get");
         try{
             if(productId == 0 || image_id == 0) {
                 throw new InvalidInputException("Enter Valid data");
             }
             productService.isAuthorisedForPut(productId,request.getHeader("Authorization").split(" ")[1], null);
+            LOGGER.info("Authentication successful for productId: "+productId+" in ImageController:fetchImageByID");
             return new ResponseEntity<>(imageService.fetchImageByID(productId, image_id), HttpStatus.OK);
         }catch (UserAuthrizationExeception e) {
+            LOGGER.warn("UserAuthrizationExeception for productId: "+productId+" in ImageController:fetchImageByID. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.FORBIDDEN);
         }catch (DataNotFoundExeception e) {
+            LOGGER.warn("No Image/product found with given details for productId: "+productId+" in ImageController:fetchImageByID. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.NOT_FOUND);
         }catch (InvalidInputException e) {
+            LOGGER.warn("Invalid Input Exception for productId: "+productId+" in ImageController:fetchImageByID. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.BAD_REQUEST);
         }
         catch(Exception e) {
             if (e instanceof DataIntegrityViolationException) {
-                //handle Internal server error 500
+                LOGGER.warn("Enter all required valid data for productId: "+productId+" in ImageController:fetchImageByID. Exception: "+e);
                 return new ResponseEntity<String>( "Enter Valid data. Make sure SKU is unique",HttpStatus.BAD_REQUEST);
             }
+            LOGGER.error("Internal server error in ImageController:fetchImageByID. Exception = {}",e);
             return new ResponseEntity<String>(UserConstants.InternalErr,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -110,24 +133,30 @@ public class ImageController {
     //delete
     @RequestMapping(path = "/v1/product/{productId}/image/{image_id}", method = RequestMethod.DELETE)
     public ResponseEntity<?> deleteImageByProductImageID(@PathVariable int productId, @PathVariable int image_id, HttpServletRequest request) {
+        statsDClient.incrementCounter("v1.product.productId.image.image_id.delete");
         try{
             if(productId == 0) {
                 throw new InvalidInputException("Enter Valid Product Id");
             }
             productService.isAuthorisedForPut(productId,request.getHeader("Authorization").split(" ")[1], null);
+            LOGGER.info("Authentication successful for productId: "+productId+" in ImageController:deleteImageByProductImageID");
             return new ResponseEntity<>(imageService.deleteFile(image_id), HttpStatus.NO_CONTENT);
         }catch (UserAuthrizationExeception e) {
+            LOGGER.warn("UserAuthrizationExeception for productId: "+productId+" in ImageController:deleteImageByProductImageID. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.FORBIDDEN);
         }catch (DataNotFoundExeception e) {
+            LOGGER.warn("No Image/product found with given details for productId: "+productId+" in ImageController:deleteImageByProductImageID. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.NOT_FOUND);
         }catch (InvalidInputException e) {
+            LOGGER.warn("Invalid Input Exception for productId: "+productId+" in ImageController:deleteImageByProductImageID. Exception: "+e);
             return new ResponseEntity<String>( e.getMessage(),HttpStatus.BAD_REQUEST);
         }
         catch(Exception e) {
             if (e instanceof DataIntegrityViolationException) {
-                //handle Internal server error 500
+                LOGGER.warn("Enter all required valid data for productId: "+productId+" in ImageController:deleteImageByProductImageID. Exception: "+e);
                 return new ResponseEntity<String>( "Enter Valid data. Make sure SKU is unique",HttpStatus.BAD_REQUEST);
             }
+            LOGGER.error("Internal server error in ImageController:deleteImageByProductImageID. Exception = {}",e);
             return new ResponseEntity<String>(UserConstants.InternalErr,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
